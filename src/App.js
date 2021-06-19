bec36bff5ec4ee3bca3b0c0156d2690760aebf45
anas40 <anasbeg50@gmail.com>
2021-06-19 15:56:40 +0530

    basic header done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './App.css';
 import Footer from './components/footer/index.js';
 import Header from './components/header/index.js';
 import Home from './pages/home.js';
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,17 @@ function Header() {
 
   return (
     <header>
-      
+      <div className="nav d-flex justify-content-between align-items-center">
+        <div className="logo-container">
+          <div className="logo"><span>V</span>acancy</div>
+        </div>
+        <div className="navbar d-flex justify-content-between ">
+          <div><a href="/">Home</a></div>
+          <div>
+            <button className="btn">Upload resume</button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
