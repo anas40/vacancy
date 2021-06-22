a0a980e2eefe0a1898a512e0dcf493d0e574c593
anas40 <anasbeg50@gmail.com>
2021-06-22 19:39:19 +0530

    copyright message and links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,16 +1,19 @@
 import { useLocation, Link } from 'react-router-dom'
 import './footer.css'
+import copyrighticon from "assets/icons/copyright.svg" 
 import UploadButton from 'components/uploadButton';
-import faqs from '../../assets/data/faq'
+import faqs from 'assets/data/faq'
 function Footer() {
   return (
     <footer>
       <section className="links">
         <Link to="/" >Home</Link>
+        <Link to="/" >Terms & Conditions</Link>
+        <Link to="/" >Privacy Policy</Link>
         <UploadButton />
       </section>
       <section className="faq-container">
-        {faqs.map((faq,index) => (
+        {faqs.map((faq, index) => (
           <section className="faq" key={index}>
             <p className="question">{faq.q}</p>
             <p className="answer">{faq.a}</p>
@@ -18,6 +21,7 @@ function Footer() {
         ))
         }
       </section>
+      <p className="copyright-message">Copyright <img src={copyrighticon} alt="copyright"/> 2021 Vacancy</p>
     </footer>
   );
 }
