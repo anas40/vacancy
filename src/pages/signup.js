fc17cac19d258eca2a47e5ab56b22ccfa743892d
anas40 <anasbeg50@gmail.com>
2021-06-24 19:21:50 +0530

    signup page setup for api call

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,27 +1,68 @@
 import 'assets/css/signup.css'
+import { useContext, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { fileContext } from 'context/fileContext'
+
+import fileIcon from 'assets/icons/fileIcon.png'
+
 function Signup() {
+  const [file] = useContext(fileContext)
+  const history = useHistory()
+  console.log(window.screen.height, window.screen.width);
   function submitForm(event) {
     event.preventDefault()
-    console.log("Form submitted");
+    const form = document.querySelector('#form')
+    const body = new FormData(form)
+    body.append('resume', file)
+
+
+    fetch('http://ec2-3-108-44-173.ap-south-1.compute.amazonaws.com:3000/upload-full', {
+      method: 'POST',
+      body
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        alert("success")
+      })
+      .catch(error => {
+        console.log(error)
+        alert("fail")
+      })
   }
+
+  useEffect(() => {
+    if (!file) history.push('/')
+  })
+
   return (
     <main>
       <div className="fullView d-flex align-items-center">
         <div className="form-container">
-          <form className="signup-form" onSubmit={submitForm}>
-            <h1>Signup Form </h1>
-          
+          <form className="signup-form" id="form" name="signup-form" onSubmit={submitForm}>
+            <h1 className="my-4">Signup Form </h1>
+            <div>
+              {
+                file && (
+                  <div className="fileContainer d-flex justify-content-center align-items-center">
+                    <img src={fileIcon} alt="Your uploaded resume" />
+                    <div>{file.name}</div>
+                  </div>
+                )
+              }
+            </div>
+
             <div className="name">
               <label htmlFor="fullName">Full Name</label>
-              <input type="text" id="fullName" required placeholder="Your full name" maxLength="50" minLength="2" />
+              <input type="text" id="fullName" name="fullname" required placeholder="Your full name" maxLength="50" minLength="2" />
             </div>
             <div className="contact">
               <label htmlFor="contact">Contact No.</label>
-              <input type="text" id="contact" required placeholder="Your 10 digit contact no." maxLength="10" minLength="10" />
+              <input type="text" id="contact" name="contact" required placeholder="Your 10 digit contact no." maxLength="10" minLength="10" />
             </div>
             <div className="email">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" required placeholder="Your email" />
+              <input type="email" id="email" name="email" required placeholder="Your email" />
             </div>
             <button className="btn d-flex align-items-center justify-content-between" type="submit">
               <span>Submit</span>
